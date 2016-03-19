f78f74fb4586495cbf925499fd06dc9ae2f17c95
capt4ce <ahmad.ali.abdilah@gmail.com>
2016-03-20 02:52:45 +0530

    commit again

--- header.php
+++ header.php
@@ -46,7 +46,7 @@
                     <li id="linkedin"><a href="#">
                         <svg width="24" height="24" viewBox="0 0 24 24 "><path class="svg-social" d="M21,21H17V14.25C17,13.19 15.81,12.31 14.75,12.31C13.69,12.31 13,13.19 13,14.25V21H9V9H13V11C13.66,9.93 15.36,9.24 16.5,9.24C19,9.24 21,11.28 21,13.75V21M7,21H3V9H7V21M5,3C6.1,3 7,3.9 7,5C7,6.1 6.1,7 5,7C3.9,7 3,6.1 3,5C3,3.9 3.9,3 5,3Z" /></svg></a>
                     </li>
-                    <li id="facebook"><a href="#">
+                    <li id="facebook"><a href="https://www.facebook.com/ahmad.a.abdilah">
                         <svg width="24" height="24" viewBox="0 0 24 24 "><path class="svg-social" d="M17,2V2H17V6H15C14.31,6 14,6.81 14,7.5V10H14L17,10V14H14V22H10V14H7V10H10V6A4,4 0 0,1 14,2H17Z"/></svg></a>
                     </li>
                     <li id="twitter"><a href="#">
